4df8999351feba6ead4aba80e6a978bb663b391b
Stuart Urback <stuart@urback.net>
2016-04-20 13:50:54 -0600

    Basic API integration working

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/api');
+var api = require('./routes/api');
 
 var app = express();
 
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var unirest = require('unirest')
+var knex = require('../lib/knex')
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  unirest.get('http://api.zippopotam.us/us/80210')
+    .end(function(response){
+      console.log(response.body);
+    })
 });
 
 module.exports = router;
