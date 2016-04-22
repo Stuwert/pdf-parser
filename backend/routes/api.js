b41d6dab154783d9d522e2dec4a25db7dc22876e
Stuart Urback <stuart@urback.net>
2016-04-22 11:41:55 -0600

    Testing hypothesis on error

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -24,47 +24,48 @@ router.get('/all', function(req, res, next) {
 });
 
 router.post('/', multipartyMiddleware, function(req, res){
-  var pdfParser = new PDFparser();
-  console.log(pdfParser);
-  pdfParser.on("pdf_dataError", function(errData){
-    console.log(errData.parserError)
-  })
-  pdfParser.on("pdfParser_dataReady", function(pdfData){
-    var pdfObj = parser(pdfData)
-    console.log(pdfObj);
-    // console.log(pdfObj.Postcode);
-    unirest.get('https://api.zippopotam.us/us/' + pdfObj.Postcode)
-    .end(function(response){
-      var responseObj = response.body;
-      if(responseObj.places){
-        var places = responseObj.places[0]
-        var dataObj = {
-          first_name : pdfObj['Forename(s)'],
-          last_name: pdfObj.Surname,
-          zip_code : pdfObj.Postcode,
-          city: places['place name'],
-          state: places.state,
-          latitude: places.latitude,
-          longitude : places.longitude
-        }
-        Users().insert(dataObj).then(function(){
-          res.status(200).json(dataObj)
-        })
-      }else{
-        res.status(206).json({message : 'Please input a correct zip code'})
-      }
-      rimraf(pathToPDF, function(){
-        console.log('finished');
-        fs.mkdir(pathToPDF, function(err){
-          console.log('bing bong');
-          if(err) throw err;
-        })
-      })
-    })
-  })
-
-  var findPDF = req.files.file.path;
-  pdfParser.loadPDF(findPDF)
+  console.log(req.files);
+  // var pdfParser = new PDFparser();
+  // console.log(pdfParser);
+  // pdfParser.on("pdf_dataError", function(errData){
+  //   console.log(errData.parserError)
+  // })
+  // pdfParser.on("pdfParser_dataReady", function(pdfData){
+  //   var pdfObj = parser(pdfData)
+  //   console.log(pdfObj);
+  //   // console.log(pdfObj.Postcode);
+  //   unirest.get('https://api.zippopotam.us/us/' + pdfObj.Postcode)
+  //   .end(function(response){
+  //     var responseObj = response.body;
+  //     if(responseObj.places){
+  //       var places = responseObj.places[0]
+  //       var dataObj = {
+  //         first_name : pdfObj['Forename(s)'],
+  //         last_name: pdfObj.Surname,
+  //         zip_code : pdfObj.Postcode,
+  //         city: places['place name'],
+  //         state: places.state,
+  //         latitude: places.latitude,
+  //         longitude : places.longitude
+  //       }
+  //       Users().insert(dataObj).then(function(){
+  //         res.status(200).json(dataObj)
+  //       })
+  //     }else{
+  //       res.status(206).json({message : 'Please input a correct zip code'})
+  //     }
+  //     rimraf(pathToPDF, function(){
+  //       console.log('finished');
+  //       fs.mkdir(pathToPDF, function(err){
+  //         console.log('bing bong');
+  //         if(err) throw err;
+  //       })
+  //     })
+  //   })
+  // })
+  //
+  // var findPDF = req.files.file.path;
+  // pdfParser.loadPDF(findPDF)
 })
 
 module.exports = router;
